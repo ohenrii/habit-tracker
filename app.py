273ba5d85cce7de48583cad5dfccc3ab4dbcddba
Henrique <ohenri.dev@gmail.com>
2025-12-29 04:29:41 -0300

    fix flash messages

--- app.py
+++ app.py
@@ -20,44 +20,42 @@ db = SQL("sqlite:///habits.db")
 
 
 @app.route("/")
+@login_required
 def index():
-    if "user_id" not in session:
-        return redirect("/login")
-
     return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
 
+    if session.get("user_id"):
+        return redirect("/")
+
     if request.method == "GET":
-        session.clear()
         return render_template("login.html")
 
-    else:
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-        username = request.form.get("username")
-        password = request.form.get("password")
+    if not username:
+        flash("Username is required", "danger")
+        return redirect("/login")
 
-        if not username:
-            flash("Username is required", "danger")
-            return redirect("/login")
+    if not password:
+        flash("Password is required", "danger")
+        return redirect("/login")
 
-        if not password:
-            flash("Password is required", "danger")
-            return redirect("/login")
+    rows = db.execute("SELECT * FROM users WHERE username = ?", username)
 
-        rows = db.execute("SELECT * FROM users WHERE username = ?", username)
+    if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
+        flash("Incorrect username or password", "danger")
+        return redirect("/login")
 
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
-            flash("Incorrect username or password", "danger")
-            return redirect("/login")
+    session["user_id"] = rows[0]["id"]
 
-        session["user_id"] = rows[0]["id"]
+    flash(f"Welcome back, {username}!", "success")
 
-        flash(f"Welcome back, {username}!", "success")
-
-        return redirect("/")
+    return redirect("/")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -66,55 +64,59 @@ def register():
     if session.get("user_id"):
         return redirect("/")
 
-    if request.method == "POST":
-
-        username = request.form.get("username")
-        password = request.form.get("password")
-        confirmation = request.form.get("confirmation")
-
-        if not username:
-            flash("Username is required", "danger")
-            return redirect("/register")
-
-        if not password:
-            flash("Password is required.", "danger")
-            return redirect("/register")
-
-        if not confirmation:
-            flash("Password confirmation is required", "danger")
-            return redirect("/register")
-
-        if password != confirmation:
-            flash("Passwords don't match", "danger")
-            return redirect("/register")
-
-        user_exists = db.execute("SELECT * FROM users WHERE username = ?", username)
-
-        if user_exists:
-            flash("Username already taken. Please choose another", "danger")
-            return redirect("/register")
-
-        password_hash = generate_password_hash(password)
-
-        try:
-            user_id = db.execute(
-                "INSERT INTO users (username, hash) VALUES (?, ?)",
-                username,
-                password_hash,
-            )
-
-            session["user_id"] = user_id
-
-            flash(f"Welcome, {username}! Account created successfully", "success")
-
-            return redirect("/")
-
-        except Exception as e:
-            flash("Error creating account. Please try again", "danger")
-            return redirect("/register")
-
-    else:
+    if request.method == "GET":
         return render_template("register.html")
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+    confirmation = request.form.get("confirmation")
+
+    if not username:
+        flash("Username is required", "danger")
+        return redirect("/register")
+
+    if not password:
+        flash("Password is required.", "danger")
+        return redirect("/register")
+
+    if not confirmation:
+        flash("Password confirmation is required", "danger")
+        return redirect("/register")
+
+    if password != confirmation:
+        flash("Passwords don't match", "danger")
+        return redirect("/register")
+
+    user_exists = db.execute("SELECT * FROM users WHERE username = ?", username)
+
+    if user_exists:
+        flash("Username already taken. Please choose another", "danger")
+        return redirect("/register")
+
+    password_hash = generate_password_hash(password)
+
+    try:
+        user_id = db.execute(
+            "INSERT INTO users (username, hash) VALUES (?, ?)",
+            username,
+            password_hash,
+        )
+
+        session["user_id"] = user_id
+
+        flash(f"Welcome, {username}! Account created successfully", "success")
+
+        return redirect("/")
+
+    except Exception as e:
+        flash("Error creating account. Please try again", "danger")
+        return redirect("/register")
+
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    session.clear()
+    return redirect("/login")
 
 
 if __name__ == "__main__":
